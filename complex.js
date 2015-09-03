0452bb53c373bc84e30a6b7d86bb2d673be2acec
mwalex <alexmuturi@gmail.com>
2015-09-04 00:41:00 +0300

    adjust indentations

--- complex.js
+++ complex.js
@@ -1,268 +1,181 @@
-
 function Complex(a, b) {
-
-if (!(this instanceof Complex)) {
-		return new Complex(a, b);
-
-}
-parse(a, b); 												//return the real and object part
-
-	this["r"] = Q["r"];
-	this["i"] = Q["i"];
-}
-/**
+    if (!(this instanceof Complex)) {
+      return new Complex(a, b);
+    }
+    parse(a, b); //return the real and object part
+    this["r"] = Q["r"];
+    this["i"] = Q["i"];
+  }
+  /**
 parse separates the string into two parts and assigns the the value
-
-
-
-
 */
-var Q = {r: 0, i: 0};
+var Q = {
+  r: 0,
+  i: 0
+};
 var parse = function(a, b) {
-
-//if a is empty sting 
-if (a === null || a === undefined) { 
-	Q["r"] = 0;
-  Q["i"] = 0;
-} else if (b !== undefined) {						// if b is empty string 
-  Q["r"] = (a); 											 //else a takes a and b
-  Q["i"] = (b);
-} else 
-
-	switch (typeof a) {
-
-	case "object":
-
-		if ("i" in a && "r" in a) {
-		    Q["r"] = (a["r"]);
-		    Q["i"] = (a["i"]);
-		} else if ("abs" in a && "arg" in a) {
-		    Q["r"] = a["abs"] * Math.cos(a["arg"]);
-		    Q["i"] = a["abs"] * Math.sin(a["arg"]);
-		} else {
-		    send_error();
-		}
-		break;
-
-	case "string":
-
-		Q["i"] = /* void */
-		Q["r"] = 0;
-
-for (var reg = /[+-]?[\di.]+/g, tmp, tr, i = 0; null !== (tmp = reg.exec(a)); i = 1) {
-
-		if (tmp[0].indexOf("i") !== -1) {//if i is first letter then --NB if expression is i alone
-
-		tr = tmp[0].replace("i", "");//if first word is i replace it with '' 
-		if (tr === "+" || tr === "-" || tr === "") //if now first leter is a operand add 1 to it 
-				tr+= "1";
-
-		Q["i"]+= parseFloat(tr);// 
-		} else {
-				Q["r"]+= parseFloat(tmp[0]);
-			}
-	}
-
-// No single iteration or nothing was sent 
-if (i === 0) {
-	send_error();
-	}
-	break;
-
-	case "number":
-		Q["i"] = 0; //only number has been in putted 
-		Q["r"] = a;
-		break;
-
-	default:
-		send_error();
-	}
-
-	if (isNaN(Q["r"] * Q["i"])) {
-			return "ERROR:Please check Numbers Passed a + ib";
-	}
+  //if a is empty sting 
+  if (a === null || a === undefined) {
+    Q["r"] = 0;
+    Q["i"] = 0;
+  } else if (b !== undefined) { // if b is empty string 
+    Q["r"] = (a); //else a takes a and b
+    Q["i"] = (b);
+  } else switch (typeof a) {
+    case "object":
+      if ("i" in a && "r" in a) {
+        Q["r"] = (a["r"]);
+        Q["i"] = (a["i"]);
+      } else if ("abs" in a && "arg" in a) {
+        Q["r"] = a["abs"] * Math.cos(a["arg"]);
+        Q["i"] = a["abs"] * Math.sin(a["arg"]);
+      } else {
+        send_error();
+      }
+      break;
+    case "string":
+      Q["i"] = /* void */ Q["r"] = 0;
+      for (var reg = /[+-]?[\di.]+/g, tmp, tr, i = 0; null !== (tmp = reg.exec(
+        a)); i = 1) {
+        if (tmp[0].indexOf("i") !== -1) { //if i is first letter then --NB if expression is i alone
+          tr = tmp[0].replace("i", ""); //if first word is i replace it with '' 
+          if (tr === "+" || tr === "-" || tr === "") //if now first leter is a operand add 1 to it 
+            tr += "1";
+          Q["i"] += parseFloat(tr); // 
+        } else {
+          Q["r"] += parseFloat(tmp[0]);
+        }
+      }
+      // No single iteration or nothing was sent 
+      if (i === 0) {
+        send_error();
+      }
+      break;
+    case "number":
+      Q["i"] = 0; //only number has been in putted 
+      Q["r"] = a;
+      break;
+    default:
+      send_error();
+  }
+  if (isNaN(Q["r"] * Q["i"])) {
+    return "ERROR:Please check Numbers Passed a + ib";
+  }
 };
 //----------------end of parse -----------------------------------------------------
-
 Complex.prototype = {
-
-	r : 0,
-	i : 0,
-// add two complex number return new complex
-	
-	add : function( real, imag ){
-		parse(real, imag);
-
-		if(imag === 0) {
-			return this['r'] + Q['r'];
-		}
-		return new Complex( this['r'] + Q['r'], this['i'] + Q['i'] );
-		
-	},
-
-//subtract complex number (r1-r2, i1-i2)
-
-	subtract : function( real, imag ) {
-		parse(real, imag);
-		return new Complex( this['r']-Q['r'],this['i'] - Q['i'] );
-	},
-
-// multiplication 
-
-multiply : function( real ,imag ) {
-	parse(real, imag);
-	
-	if(imag === 0) {
-			return (this["r"] *Q["r"]) - (this["i"] * Q["i"]);
-		}
-
-	return new Complex( (this["r"] *Q["r"]) - (this["i"] * Q["i"]),
-                    	(this["r"] * Q["i"] )+ (this["i"] * Q["r"]) );
-},
-//square
-
-
-
-// division
-
-divide : function( real, imag) {
-parse(real, imag);	
-	var x = Q["i"] ,
-		  y = Q["r"],
-		  a,
-		  b,
-		  d= x * x + y * y;
-	
-	
-	//check if the denominator is 0 cannot divide 0
-	if (d === 0) {
-
-		 return 'cannot divide';
-	}
-
- a = x * this["r"] + y* this["i"]/ d ;
- b = x * this["i"] - y * this["r"] / d;
-	
-
-	  return new Complex( parseFloat(parseFloat(a)).toFixed(5), b);
-},
-
-//find magnitude this use pathogoras thorem 3+4i=5
-abs: function() {
-	var a = this ['r'];
-	var b = this['i'];
-
-	return Math.sqrt(a*a + b*b);
-},
-
-
-// squareRooot 
-
-sqrt: function () {
-var H = 1;
-var r = this['abs']();
-
-	if (this['i'] < 0) {
-		H = -1;
-	}	
-
-	return new Complex(
-                    Math.sqrt((r + this["r"]) / 2),
-                    Math.sqrt((r - this["r"]) / 2) * H
-                    );
-
-},
-
-exp : function() {
-
-            var tmp = Math.exp(this["r"]);
-
-            return new Complex(
-                    tmp * Math.cos(this["i"]),
-                    tmp * Math.sin(this["i"]));
-        },
-
-pow: function(a, b) {
-
-            parse(a, b);
-
-            a = this["r"];
-            b = this["i"];
-
-            var abs = a * a + b * b;
-            var arg = Math.atan2(b, a);
-
-            if (abs === 0) {
-                return new Complex(0);
-            }
-
-            a = Math.pow(abs, Q["r"] / 2) * Math.exp(-Q["i"] * arg);
-            b = Q["i"] * Math.log(abs) / 2 + Q["r"] * arg;
-
-            return new Complex(
-                    a * Math.cos(b),
-                    a * Math.sin(b)
-                    );
-        },
-conjugate : function() {
-
-            return new Complex(this["r"], -1*this["i"]);
-        },
-cos : function() {
-
-		   var cosh = function(x) {
-        return (Math.exp(x) + Math.exp(-x)) / 2;
+  r: 0,
+  i: 0,
+  // add two complex number return new complex
+  add: function(real, imag) {
+    parse(real, imag);
+    if (imag === 0) {
+      return this['r'] + Q['r'];
+    }
+    return new Complex(this['r'] + Q['r'], this['i'] + Q['i']);
+  },
+  //subtract complex number (r1-r2, i1-i2)
+  subtract: function(real, imag) {
+    parse(real, imag);
+    return new Complex(this['r'] - Q['r'], this['i'] - Q['i']);
+  },
+  // multiplication 
+  multiply: function(real, imag) {
+    parse(real, imag);
+    if (imag === 0) {
+      return (this["r"] * Q["r"]) - (this["i"] * Q["i"]);
+    }
+    return new Complex((this["r"] * Q["r"]) - (this["i"] * Q["i"]), (this[
+      "r"] * Q["i"]) + (this["i"] * Q["r"]));
+  },
+  //square
+  // division
+  divide: function(real, imag) {
+    parse(real, imag);
+    var x = Q["i"],
+      y = Q["r"],
+      a,
+      b,
+      d = x * x + y * y;
+    //check if the denominator is 0 cannot divide 0
+    if (d === 0) {
+      return 'cannot divide';
+    }
+    a = x * this["r"] + y * this["i"] / d;
+    b = x * this["i"] - y * this["r"] / d;
+    return new Complex(parseFloat(parseFloat(a)).toFixed(5), b);
+  },
+  //find magnitude this use pathogoras thorem 3+4i=5
+  abs: function() {
+    var a = this['r'];
+    var b = this['i'];
+    return Math.sqrt(a * a + b * b);
+  },
+  // squareRooot 
+  sqrt: function() {
+    var H = 1;
+    var r = this['abs']();
+    if (this['i'] < 0) {
+      H = -1;
+    }
+    return new Complex(Math.sqrt((r + this["r"]) / 2), Math.sqrt((r - this[
+      "r"]) / 2) * H);
+  },
+  exp: function() {
+    var tmp = Math.exp(this["r"]);
+    return new Complex(tmp * Math.cos(this["i"]), tmp * Math.sin(this["i"]));
+  },
+  pow: function(a, b) {
+    parse(a, b);
+    a = this["r"];
+    b = this["i"];
+    var abs = a * a + b * b;
+    var arg = Math.atan2(b, a);
+    if (abs === 0) {
+      return new Complex(0);
+    }
+    a = Math.pow(abs, Q["r"] / 2) * Math.exp(-Q["i"] * arg);
+    b = Q["i"] * Math.log(abs) / 2 + Q["r"] * arg;
+    return new Complex(a * Math.cos(b), a * Math.sin(b));
+  },
+  conjugate: function() {
+    return new Complex(this["r"], -1 * this["i"]);
+  },
+  cos: function() {
+    var cosh = function(x) {
+      return (Math.exp(x) + Math.exp(-x)) / 2;
     };
-
-
-},
-
-arg : function() {
-
-            return Math.atan2(this["i"], this["r"]);
-        },
-
-
-
-
- toString: function() {
-
-            var a = this["r"];
-            var b = this["i"];
-
-            if (isNaN(a * b)) {
-                return "NaN";
-            }
-
-            var ret = "";
-
-            if (a !== 0) {
-                ret+= a;
-            }
-
-            if (b !== 0) {
-
-                if (b > 0 && a !== 0)
-                    ret+= "+";
-
-                if (b === 1) {
-
-                } else if (b === -1) {
-                    ret+= "-";
-                } else {
-                    ret+= b;
-                }
-                ret+= "i";
-            }
-
-            if (ret === "")
-                return "0";
-
-            return ret;
-        },
-
-
+  },
+  arg: function() {
+    return Math.atan2(this["i"], this["r"]);
+  },
+  toString: function() {
+    var a = this["r"];
+    var b = this["i"];
+    if (isNaN(a * b)) {
+      return "NaN";
+    }
+    var ret = "";
+    if (a !== 0) {
+      ret += a;
+    }
+    if (b !== 0) {
+      if (b > 0 && a !== 0) ret += "+";
+      if (b === 1) {
+        
+      } else if (b === -1) {
+        ret += "-";
+      } else {
+        ret += b;
+      }
+      ret += "i";
+    }
+    if (ret === "") return "0";
+    return ret;
+  },
 }
+
 function send_error() {
-	return 'ERROR insert complex numbers'
+  return 'ERROR insert complex numbers'
 }
+exports.Complex = Complex;
--- spec/ComplexFunction.js
+++ spec/ComplexFunction.js
@@ -1,78 +1,58 @@
 //http://www.mathopolis.com/questions/q.php?id=443 <------used experimental equestion from here 
 describe("Complex Numbers Library", function() {
 
-	it("should return a string expression", function() {
-		var numx = new Complex(3, 4);
-		expect(numx.toString()).toEqual("3+4i");
-	});
+  it("should return a string expression", function() {
+    var numx = new Complex(3, 4);
+    expect(numx.toString()).toEqual("3+4i");
+  });
 
-	it("should return an object ", function() {
-		var numx = new Complex('3+4i');
-		expect(numx).toEqual({r: 3,i: 4});
-	});
+  it("should return an object ", function() {
+    var numx = new Complex('3+4i');
+    expect(numx).toEqual({r: 3, i: 4});
+  });
 
-	it("should find the Magnitude check pythogras", function(){
-		var numx = new Complex(4, 5);
-		expect(numx.abs()).toEqual(Math.sqrt(41));
-		expect(new Complex(2,4).abs()).toEqual(Math.sqrt(20));
-		expect(new Complex('2+4i').abs()).toEqual(Math.sqrt(20));
-	});
-//check addition 
-	it("should find addition of two complex numbers ", function() {
-		var numx = new Complex('3+i').add('2+2i');
-		expect(numx.toString()).toEqual('5+3i');
-	});
+  it("should find the Magnitude check pythogras", function() {
+    var numx = new Complex(4, 5);
+    expect(numx.abs()).toEqual(Math.sqrt(41));
+    expect(new Complex(2, 4).abs()).toEqual(Math.sqrt(20));
+    expect(new Complex('2+4i').abs()).toEqual(Math.sqrt(20));
+  });
+  //check addition 
+  it("should find addition of two complex numbers ", function() {
+    var numx = new Complex('3+i').add('2+2i');
+    expect(numx.toString()).toEqual('5+3i');
+  });
 
-	it("should find subtraction of two complex numbers ", function() {
-		var numx = new Complex('3+i').subtract('2+2i');
-		expect(numx.toString()).toEqual('1-i');
-	});
-	// check multiplication for both numeric and string value 
-	it("should find product of two complex numbers ", function() {
-		var numx = new Complex('2-3i').multiply('1+i');
-		expect(numx.toString()).toEqual('5-i');
+  it("should find subtraction of two complex numbers ", function() {
+    var numx = new Complex('3+i').subtract('2+2i');
+    expect(numx.toString()).toEqual('1-i');
+  });
+  // check multiplication for both numeric and string value 
+  it("should find product of two complex numbers ", function() {
+    var numx = new Complex('2-3i').multiply('1+i');
+    expect(numx.toString()).toEqual('5-i');
 
-		var numx = new Complex(2,-3).multiply(1,1);
-		expect(numx.toString()).toEqual('5-i');
-	});
+    numx = new Complex(2, -3).multiply(1, 1);
+    expect(numx.toString()).toEqual('5-i');
+  });
 
-	it("Should find Square of a complex number", function() {
-		 var numx = new Complex('3-i').pow(2);
-		 expect( numx.toString()).toEqual('8-6i');
-		 	 var numx = new Complex('3-i').pow(3);
-		  var numx = new Complex('3-i').pow('18-26i');
-		   
-
-	});
-
-	it('should return the conjugate', function(){
-		expect(new Complex(1, 3).conjugate().toString()).toEqual('1-3i');
-
-	});
-
-	it('should z^w, where z and w are complex', function(){
-<<<<<<< HEAD
-		var n = new Complex('2').pow(2).toString();
-		expect(n).toEqual('4');
-	});
+  it("Should find Square of a complex number", function() {
+    var numx = new Complex('3-i').pow(2);
+    expect(numx.toString()).toEqual('8-6i');
+     numx = new Complex('3-i').pow(3);
+     numx = new Complex('3-i').pow('18-26i');
 
 
+  });
 
+  it('should return the conjugate', function() {
+    expect(new Complex(1, 3).conjugate().toString()).toEqual('1-3i');
+
+  });
+
+  it('should z^w, where z and w are complex', function() {
+    var n = new Complex('2').pow(2).toString();
+    expect(n).toEqual('4');
+  });
 
 })
-module.exports.Complex = Complex;
-=======
-		var n = new Complex(1, 2).pow(new Complex(3, 4)).toPrecision(10).toString();
-		expect(n).to.equal('0.1290095941+0.03392409291i');
-	});
-
-it('should test the equals method', function(){
-		expect(new Complex(2, 3).equals(new Complex(2, 3))).to.be.ok();
-		expect(new Complex(2, 3).equals(new Complex(2, 4))).not.to.be.ok();
-		expect(new Complex(2, 3).equals(new Complex(1, 3))).not.to.be.ok();
-	});
-
-
-
-})
->>>>>>> f71dfcdf5b263b3bbaf2379890f736a5dcd17150
